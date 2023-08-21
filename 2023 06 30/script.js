85a95c870bdba9be17090bf88f82781d22b3454a
K. L <121451594+madparrot69@users.noreply.github.com>
2023-08-21 20:38:45 +0300

    additional task

---
+++ 2023 06 30/script.js
@@ -0,0 +1,43 @@
+// console.log(false == 0);
+
+// let isCheck = confirm('No');
+
+// if(isCheck){   
+
+//     console.log('ok');
+// } 
+// else{
+
+//     console.log('error');
+// }
+
+// 1
+// let checkPoint = +prompt(`how old are you?`)
+// console.log(checkPoint);
+// if(checkPoint >= 18){
+//     alert(`you're grown up person.`);
+// }
+// else{
+//     alert(`you're teenager.`);
+// }
+
+// 2
+// let password = +prompt(`enter you're password:`);
+// let passwordCheck = +prompt(`repeat you're password:`);
+
+// if(password === passwordCheck){
+//     alert(`correct password.`);
+// }
+// else{
+//     +prompt(`incorrect password. try again now:`)
+// }
+
+//3
+let number = +prompt(`enter number`);
+console.log(number);
+if(number % 2 === 0){
+    alert(`even number`);
+}
+else{
+    alert(`odd number`);
+}

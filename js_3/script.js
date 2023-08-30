bd1dcc9c06f58c07d6e9497e93c778d5d1fc8ab6
K. L <121451594+madparrot69@users.noreply.github.com>
2023-08-30 18:11:37 +0300

    js_3

---
+++ js_3/script.js
@@ -0,0 +1,32 @@
+//1
+
+let result = 5 + 5 + '5';
+
+console.log(typeof result);
+console.log(result);
+
+//2
+
+let email = `k.lokidottir@gmail.com`;
+
+console.log(email.includes(`@`));
+console.log(email.length);
+
+//3
+
+let m = `my`;
+let n = `name`;
+let i = `is`;
+
+let fullName = m +" "+ n +" "+ i;
+fullName += ` Victor`;
+
+console.log(fullName);
+
+// 4
+
+let userName = ` Khrystyna`;
+
+let payment = 60;
+
+alert(`thank you,` + userName +`! you need to pay `+ payment + ` UAH.`);

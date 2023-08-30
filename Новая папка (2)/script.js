56fc2786d193a17c8eee4955797751b2cf3ca322
K. L <121451594+madparrot69@users.noreply.github.com>
2023-08-30 20:48:19 +0300

    hz

---
+++ Новая папка (2)/script.js
@@ -0,0 +1,63 @@
+// // var moneyBank = 25;
+
+// // console.log(typeof moneyBank);
+
+// // let name = "Khrystyna";
+// // console.log(typeof name.toUpperCase());
+// // console.log(typeof name.length());
+
+// // const BACKGROUND_COLOR = "black";
+
+// // let a = 5;
+
+// // let b = 6;
+
+// // console.log(a + b);
+
+// // console.log(a - b);
+
+// // console.log(a * b);
+
+// // console.log(a / b);
+
+// // console.log(a % b);
+
+// // let number = 10;
+
+// // let num2 = 2;
+
+// // // number = number + num2;
+
+// // number += num2;
+
+// // // console.log(number);
+
+// // let number = +prompt("Press is number");
+
+// // consol.log( "Тип данних:", typeof number);
+
+// // console.log(number);
+
+
+// let age = +prompt("Введіть ваш вік ");
+
+
+// if (age < 18){
+//     alert ("ви неповнолітня людина");
+// } if (age > 21){
+//     alert ("ви повнолітня людина");
+// }
+
+// console.log(age)
+
+let code = +prompt("Введіть пароль");
+let code2 = +prompt("Підтвердіть пароль");
+
+if (code == code2){
+    alert("паролі збігаються");
+} else{
+    alert ("паролі не збігються");
+}
+
+console.log(code);
+console.log(code2);
